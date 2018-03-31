86b2b43fdabc65fbdc4e0778d42c1994ccd2b024
Igor Ibit <ibit.igor@gmail.com>
2018-03-31 17:16:51 +0000

    models and db config

---
+++ common/models/user.js
@@ -0,0 +1,22 @@
+// Copyright IBM Corp. 2015,2016. All Rights Reserved.
+// Node module: loopback-example-access-control
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
+module.exports = function(User) {
+    User.getName = function(userId, cb) {
+        User.findById(userId, function(err, instance) {
+            var response = "Name of user " + instance.name;
+            cb(null, response);
+            console.log(response);
+        });
+    };
+
+    User.remoteMethod(
+        'getName', {
+            http: { path: '/getname', verb: 'get' },
+            accepts: { arg: 'id', type: 'number', http: { source: 'query' } },
+            returns: { arg: 'name', type: 'string' }
+        }
+    );
+};
