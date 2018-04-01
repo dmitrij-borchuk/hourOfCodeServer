0ab622375e936add0015e0744b0fa5ec1733df1d
Igor Ibit <ibit.igor@gmail.com>
2018-04-01 11:43:19 +0000

    user roles, data changes

--- server/boot/initial-data.js
+++ server/boot/initial-data.js
@@ -25,30 +25,33 @@ module.exports = function(app) {
   ];
 
   var schoolsData = [
-    { name: 'СШ 1', lat: '49.841116', lng: "23.974943", districtId: 0 },
-    { name: 'СШ 2', lat: '49.841116', lng: "23.974943", districtId: 0 },
-    { name: 'СШ 3', lat: '49.841116', lng: "23.974943", districtId: 0 },
-    { name: 'СШ 11', lat: '49.841116', lng: "23.974943", districtId: 1 },
-    { name: 'СШ 12', lat: '49.841116', lng: "23.974943", districtId: 1 },
-    { name: 'СШ 13', lat: '49.841116', lng: "23.974943", districtId: 1 },
+    { name: 'СШ 1', lat: '49.841116', lng: "23.974943", districtId: 1 },
+    { name: 'СШ 2', lat: '49.841116', lng: "23.974943", districtId: 1 },
+    { name: 'СШ 3', lat: '49.841116', lng: "23.974943", districtId: 1 },
+    { name: 'СШ 11', lat: '49.841116', lng: "23.974943", districtId: 2 },
+    { name: 'СШ 12', lat: '49.841116', lng: "23.974943", districtId: 2 },
+    { name: 'СШ 13', lat: '49.841116', lng: "23.974943", districtId: 2 },
     { name: 'СШ 21', lat: '49.841116', lng: "23.974943", districtId: 3 },
     { name: 'СШ 22', lat: '49.841116', lng: "23.974943", districtId: 3 },
     { name: 'СШ 23', lat: '49.841116', lng: "23.974943", districtId: 3 }
   ];
 
   var groupsData = [
-    { name: 'Клас 8А', dateStart: '2018-03-31T17:59:08.482Z', dateEnd: "2018-03-31T17:59:08.482Z", details: "15 компютерів, принтер", mentorId: 6, teacherId: 2, schoolId: 1 }, //mentorId teacherId schoolId
-    { name: 'Клас 9А', dateStart: '2018-03-31T17:59:08.482Z', dateEnd: "2018-03-31T17:59:08.482Z", details: "5 компютерів", mentorId: 7, teacherId: 3, schoolId: 1 },
-    { name: 'Клас 10А 1', dateStart: '2018-03-31T17:59:08.482Z', dateEnd: "2018-03-31T17:59:08.482Z", details: "10 компютерів, інтернет", mentorId: 0, teacherId: 4, schoolId: 1 },
-    { name: 'Клас 10А 2', dateStart: '2018-03-31T17:59:08.482Z', dateEnd: "2018-03-31T17:59:08.482Z", details: "10 компютерів, інтернет", mentorId: 6, teacherId: 3, schoolId: 1 },
-    { name: 'Клас 18А', dateStart: '2018-03-31T17:59:08.482Z', dateEnd: "2018-03-31T17:59:08.482Z", details: "15 компютерів, принтер", mentorId: 6, teacherId: 4, schoolId: 2 }, //mentorId teacherId schoolId
-    { name: 'Клас 19А', dateStart: '2018-03-31T17:59:08.482Z', dateEnd: "2018-03-31T17:59:08.482Z", details: "5 компютерів", mentorId: 0, teacherId: 7, schoolId: 2 }, 
-    { name: 'Клас 110А 1', dateStart: '2018-03-31T17:59:08.482Z', dateEnd: "2018-03-31T17:59:08.482Z", details: "10 компютерів, інтернет", mentorId: 8, teacherId: 2, schoolId: 2 },
-    { name: 'Клас 110А 2', dateStart: '2018-03-31T17:59:08.482Z', dateEnd: "2018-03-31T17:59:08.482Z", details: "10 компютерів, інтернет", mentorId: 9, teacherId: 3, schoolId: 2 },
-    { name: 'Клас 28А', dateStart: '2018-03-31T17:59:08.482Z', dateEnd: "2018-03-31T17:59:08.482Z", details: "15 компютерів, принтер", mentorId: 6, teacherId: 5, schoolId: 3}, //mentorId teacherId schoolId
-    { name: 'Клас 29А', dateStart: '2018-03-31T17:59:08.482Z', dateEnd: "2018-03-31T17:59:08.482Z", details: "5 компютерів", mentorId: 0, teacherId: 7, schoolId: 3 }, 
-    { name: 'Клас 210А 1', dateStart: '2018-03-31T17:59:08.482Z', dateEnd: "2018-03-31T17:59:08.482Z", details: "10 компютерів, інтернет", mentorId: 0, teacherId: 5, schoolId: 3 },
-    { name: 'Клас 210А 2', dateStart: '2018-03-31T17:59:08.482Z', dateEnd: "2018-03-31T17:59:08.482Z", details: "10 компютерів, інтернет", mentorId: 0, teacherId: 4, schoolId: 3 }
+    { name: 'Клас 8А', dateStart: '2018-12-03T09:00:08.482Z', dateEnd: "2018-12-03T11:00:08.482Z", details: "15 компютерів, принтер", mentorId: 6, teacherId: 2, schoolId: 1 }, //mentorId teacherId schoolId
+    { name: 'Клас 9А', dateStart: '2018-12-04T13:00:08.482Z', dateEnd: "2018-12-03T14:00:08.482Z", details: "5 компютерів", mentorId: 7, teacherId: 3, schoolId: 1 },
+    { name: 'Клас 10А 1', dateStart: '2018-12-05T09:00:08.482Z', dateEnd: "2018-12-03T11:00:08.482Z", details: "10 компютерів, інтернет", mentorId: 0, teacherId: 4, schoolId: 1 },
+    { name: 'Клас 10А 2', dateStart: '2018-12-05T12:00:08.482Z', dateEnd: "2018-12-03T13:00:08.482Z", details: "12 компютерів, інтернет", mentorId: 6, teacherId: 3, schoolId: 1 },
+    { name: 'Клас 18А', dateStart: '2018-12-03T09:00:08.482Z', dateEnd: "2018-12-03T12:00:08.482Z", details: "15 компютерів, принтер", mentorId: 6, teacherId: 4, schoolId: 2 }, //mentorId teacherId schoolId
+    { name: 'Клас 19А', dateStart: '2018-12-03T09:00:08.482Z', dateEnd: "2018-12-03T09:00:08.482Z", details: "5 компютерів", mentorId: 0, teacherId: 7, schoolId: 2 }, 
+    { name: 'Клас 110А 1', dateStart: '2018-12-03T09:00:08.482Z', dateEnd: "2018-12-03T09:00:08.482Z", details: "10 компютерів, інтернет", mentorId: 8, teacherId: 2, schoolId: 2 },
+    { name: 'Клас 110А 2', dateStart: '2018-12-03T09:00:08.482Z', dateEnd: "2018-12-03T09:00:08.482Z", details: "10 компютерів, інтернет", mentorId: 9, teacherId: 3, schoolId: 2 },
+    { name: 'Клас 28А', dateStart: '2018-12-03T09:00:08.482Z', dateEnd: "2018-12-03T09:00:08.482Z", details: "15 компютерів, принтер", mentorId: 6, teacherId: 5, schoolId: 3}, //mentorId teacherId schoolId
+    { name: 'Клас 29А', dateStart: '2018-12-03T09:00:08.482Z', dateEnd: "2018-12-03T09:00:08.482Z", details: "5 компютерів", mentorId: 0, teacherId: 7, schoolId: 3 }, 
+    { name: 'Клас 210А 1', dateStart: '2018-12-03T09:00:08.482Z', dateEnd: "2018-12-03T09:00:08.482Z", details: "10 компютерів, інтернет", mentorId: 0, teacherId: 5, schoolId: 3 },
+    { name: 'Клас 210А 2', dateStart: '2018-12-03T09:00:08.482Z', dateEnd: "2018-12-03T09:00:08.482Z", details: "10 компютерів, інтернет", mentorId: 0, teacherId: 4, schoolId: 3 },
+    { name: 'Клас 29А', dateStart: '2018-12-03T09:00:08.482Z', dateEnd: "2018-12-03T09:00:08.482Z", details: "5 компютерів", mentorId: 0, teacherId: 7, schoolId: 4 }, 
+    { name: 'Клас 210А 1', dateStart: '2018-12-03T09:00:08.482Z', dateEnd: "2018-12-03T09:00:08.482Z", details: "10 компютерів, інтернет", mentorId: 0, teacherId: 5, schoolId: 4 },
+    { name: 'Клас 210А 2', dateStart: '2018-12-03T09:00:08.482Z', dateEnd: "2018-12-03T09:00:08.482Z", details: "10 компютерів, інтернет", mentorId: 0, teacherId: 4, schoolId: 4 }
   ];
 
   usersData.forEach(function(item) {
